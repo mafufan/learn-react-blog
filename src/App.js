6f5325e3ce33efcf80e252f5b68ff0c632baf398
huijae <mafufan@naver.com>
2022-05-21 21:46:49 +0900

    onClick 개별 동작하도록 만들기

--- src/App.js
+++ src/App.js
@@ -9,61 +9,71 @@ function App() {
     '강남 우동맛집',
     '파이썬 독학',
   ]);
-  let [modal, setModal] = useState(false);
-  let [따봉, 따봉변경] = useState(0);
+
+  let modal배열 = [];
+  글제목.map(function (a, i) {
+    modal배열.push(false);
+  });
+  let [modal, setModal] = useState(modal배열);
+  // let [modal, setModal] = useState([false, false, false]);
+
+  let 따봉배열 = [];
+  글제목.map(function (a, i) {
+    따봉배열.push(0);
+  });
+  let [따봉, 따봉변경] = useState(따봉배열);
+  // let [따봉, 따봉변경] = useState(0,0,0);
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
-      <div className="list">
-        <button
-          onClick={() => {
-            let copy = [...글제목];
-            copy.sort();
-            글제목변경(copy);
-          }}
-        >
-          정렬 버튼
-        </button>
-        <button
-          onClick={() => {
-            let copy = [...글제목];
-            copy[0] = '여자 코트 추천';
-            글제목변경(copy);
-          }}
-        >
-          수정
-        </button>
-        <h4>
-          {글제목[0]}
-          <span
-            onClick={() => {
-              따봉변경(따봉 + 1);
-            }}
-          >
-            👍
-          </span>
-          {따봉}
-        </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{글제목[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4
-          onClick={() => {
-            setModal(!modal);
-          }}
-        >
-          {글제목[2]}
-        </h4>
-        <p>2월 17일 발행</p>
-        {modal == true ? <Modal /> : null}
-      </div>
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy.sort();
+          글제목변경(copy);
+        }}
+      >
+        정렬 버튼
+      </button>
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy[0] = '여자 코트 추천';
+          글제목변경(copy);
+        }}
+      >
+        수정
+      </button>
+      {글제목.map(function (a, i) {
+        return (
+          <div className="list" key="i">
+            <h4
+              onClick={() => {
+                let copy = [...modal];
+                copy[i] = copy[i] ? false : true;
+                setModal(copy);
+              }}
+            >
+              {글제목[i]}
+              <span
+                onClick={() => {
+                  let copy = [...따봉];
+                  copy[i]++;
+                  따봉변경(copy);
+                }}
+              >
+                👍
+              </span>
+              {따봉[i]}
+            </h4>
+            <p>2월 18일 발행</p>
+            {modal[i] == true ? <Modal /> : null}
+          </div>
+        );
+      })}
     </div>
   );
 }

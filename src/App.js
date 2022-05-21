2a05ac175c46c053bfb1fa24b32a3dee86e1676a
huijae <mafufan@naver.com>
2022-05-21 17:36:04 +0900

    Create Component Function Modal

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
+  let [글제목, 글제목변경] = useState([
+    '남자 코트 추천',
+    '강남 우동맛집',
+    '파이썬 독학',
+  ]);
 
   let [따봉, 따봉변경] = useState(0);
 
@@ -52,6 +56,20 @@ function App() {
       <div className="list">
         <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
+      </div>
+
+      <Modal></Modal>
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div>
+      <div className="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
       </div>
     </div>
   );
